14f761560226ef1bb7943e8c60d9dd5566b706b3
Morgan Herlocker <morgan.herlocker@gmail.com>
2014-03-21 14:38:33 -0400

    reading level working

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-var textStats = require('text-statistics')
+var ts = require('text-statistics')(),
+    fs = require('fs')
+
+var files = fs.readdirSync('./samples/')
+
+files.forEach(function(file){
+  console.log(file)
+  var text = fs.readFileSync('./samples/'+file).toString()
+
+  console.log('==================')
+  console.log('Name: '+ file.split('.')[0])
+  var syllables = ts.syllableCount(text)
+  var grade = ts.fleschKincaidGradeLevel(text)
+  console.log('grade: '+grade)
+})
